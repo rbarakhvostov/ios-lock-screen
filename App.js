f7310652ee42705ec4a3b882501818ecb68edf3e
rbarakhvostov <rbarakhvostov@gmail.com>
2023-06-21 15:27:38 +0300

    dynamic date & time

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ImageBackground, View } from 'react-native';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import dayjs from 'dayjs';
 import wallpaper from './assets/images/wallpaper.webp';
 
 export default function App() {
+  const [date, setDate] = useState(dayjs());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(dayjs())
+    }, 1000);
+
+    return () => clearInterval(interval);
+  })
+
   return (
     <ImageBackground source={wallpaper} style={styles.container}>
       <View style={styles.header}>
         <Ionicons name="ios-lock-closed" size={20} color="#ffffff" />
-        <Text style={styles.date}>Tuesday, 20 June</Text>
-        <Text style={styles.time}>22:00</Text>
+        <Text style={styles.date}>{date.format("dddd, DD MMMM")}</Text>
+        <Text style={styles.time}>{date.format("hh:mm:ss")}</Text>
       </View>
 
       <View style={styles.footer}>
@@ -42,7 +54,7 @@ const styles = StyleSheet.create({
     color: "#c3fffe"
   },
   time: {
-    fontSize: 82,
+    fontSize: 72,
     fontWeight: 700,
     color: "#c3fffe"
   },
